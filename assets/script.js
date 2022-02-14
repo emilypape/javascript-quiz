e41525f0b6fdaca97d41f0ffc71c501f249e2d6c
emily <eap6787@gmail.com>
2022-02-13 18:08:25 -0700

    first question display

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
 var startQuizBtnEL = document.querySelector('#startBtn');
 var clearWelcomePgEl = document.querySelector('#startPage');
 var questionContainer = document.querySelector('#question-container');
+var bodyEl = document.querySelector('#bodyEL');
 
 
 function startQuiz() {
@@ -9,7 +10,38 @@ function startQuiz() {
 }
 
 function question1() {
-    console.log('Question 1')
+    var questionBox1 = document.createElement('div');
+    questionBox1.className = "questionStyles";
+    questionBox1.textContent = "How do you write an ID in Javascript?";
+    questionContainer.appendChild(questionBox1);
+
+    var answerBoxObj = {
+        answer1: '1. .idName',
+        answer2: '2. -idName',
+        answer3: '3. #idName',
+        answer4: '4. idName'
+    }
+
+    var answerBox1 = document.createElement('button');
+    answerBox1.className = "purpleAnswerBtn";
+    answerBox1.textContent = answerBoxObj.answer1;
+    questionBox1.appendChild(answerBox1);
+
+    var answerBox2 = document.createElement('button');
+    answerBox2.className = "purpleAnswerBtn";
+    answerBox2.textContent = answerBoxObj.answer2;
+    questionBox1.appendChild(answerBox2);
+
+    var answerBox3 = document.createElement('button');
+    answerBox3.className = "purpleAnswerBtn";
+    answerBox3.textContent = answerBoxObj.answer3;
+    questionBox1.appendChild(answerBox3);
+
+    var answerBox4 = document.createElement('button');
+    answerBox4.className = "purpleAnswerBtn";
+    answerBox4.textContent = answerBoxObj.answer4;
+    questionBox1.appendChild(answerBox4);
+
     // ask question - put all the html where it needs to be
 
     // check if right when user clicks answer
